7f8b46fd256a41d10f338c53507b3ef8f3e264d3
mmalinova <malinova29@gmail.com>
2022-08-18 14:06:24 +0300

    Initial setup

---
+++ src/main/java/bg/project/proteinstructuresimilaritydetection/ProteinStructureSimilarityDetectionApplication.java
@@ -0,0 +1,13 @@
+package bg.project.proteinstructuresimilaritydetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProteinStructureSimilarityDetectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProteinStructureSimilarityDetectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bg/project/proteinstructuresimilaritydetection/ProteinStructureSimilarityDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.project.proteinstructuresimilaritydetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProteinStructureSimilarityDetectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
